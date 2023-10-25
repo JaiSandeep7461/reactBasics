3286090da113bd0688c17272dc29d18fdfd86611
JaiSandeep7461 <jaisandeep7461@gmail.com>
2023-10-25 20:54:15 +0530

    Learned about views and how to give styling to components using inline css

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Iam Sandy Android Developer This is Sample</Text>
-      <StatusBar style="auto" />
+  return <View style={{ flex: 1, backgroundColor: "plum" }}>
+    <View style={{ width: 200, height: 200, backgroundColor: "lightblue" }}>
     </View>
-  );
+    <View style={{width:200,height:200 , backgroundColor: "lightgreen"}}>
+     </View>
+  </View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
